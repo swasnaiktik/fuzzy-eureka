a2914fdb0cdf288238eaa19ef03319a24769de76
Swastik Naik <swasnaiktik@gmail.com>
2022-01-08 03:38:22 -0500

    Shipment creation, display and item display functions working

--- App/shipment.py
+++ App/shipment.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, redirect
-from .db import InventoryItem
+from flask import Blueprint, render_template, request, redirect, flash
+from .db import db, InventoryItem, Shipment, ShipmentItems
 shipment = Blueprint("shipment", __name__, template_folder='templates', static_folder='static', url_prefix='/shipment')
 
 
@@ -7,6 +7,22 @@ shipment = Blueprint("shipment", __name__, template_folder='templates', static_f
 def shipmentLanding():
     if request.method == 'GET':
         inventoryItems = InventoryItem.query.all() or []
-        return render_template("shipment.html", itemList=inventoryItems)
+        shipmentList = Shipment.query.all()
+        return render_template("shipment.html", itemList=inventoryItems, shipmentList=shipmentList)
     else:
+        try:
+            shipment = Shipment(name=request.form['Name'])
+            items = InventoryItem.query.all()
+            for item in items:
+                item_quantity = request.form[item.name]
+                if item_quantity:
+                    ship = ShipmentItems(item=item, quantity=item_quantity)
+                    shipment.items.append(ship)
+                    db.session.add(ship)
+            db.session.add(shipment)
+            db.session.commit()
+            flash("Successfully to Created a Shipment")
+        except:
+            flash("Failed to Create a Shipment")
+
         return redirect('/shipment/')
